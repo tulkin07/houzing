c47d4332bd701e32ca612d40a5d5b4ed3bd78e8d
tulkin07 <akhmadov0770@gmail.com>
2023-04-10 14:52:05 +0500

    router

--- src/root/index.jsx
+++ src/root/index.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom'
 import { navbar } from '../utils/navbar'
 import Navbar from '../components/Navbar'
+import HomePage from '../pages/Home'
 
 export default function Root() {
   return (
@@ -11,7 +12,9 @@ export default function Root() {
           {
             navbar.map(value => <Route key={value.id} path={value.path} element={value.element} />)
           }
+          <Route path='/' element={<HomePage/>}/>
         </Route>
+        
         <Route path='*' element={<div>404 not found</div>} />
       </Routes>
     </BrowserRouter>
